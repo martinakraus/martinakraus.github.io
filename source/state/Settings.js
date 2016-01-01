cce96374274f95f05e8fc36e9449afe90803fffa
Martina Kraus <wing121@web.de>
2016-01-02 00:44:17 +0100

    initial commit

---
+++ source/state/Settings.js
@@ -0,0 +1,23 @@
+
+lychee.define('app.state.Settings').includes([
+	'lychee.app.State'
+]).tags({
+	platform: 'html'
+}).exports(function(lychee, app, global, attachments) {
+
+	var Class = function(main) {
+
+		lychee.app.State.call(this, main);
+
+	};
+
+
+	Class.prototype = {
+
+	};
+
+
+	return Class;
+
+});
+
